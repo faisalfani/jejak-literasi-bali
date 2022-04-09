59dd6659c23da3c0f08f7d8fb3e32b0241f31c8f
faisal isfahani <workwfaisal@gmail.com>
2022-04-09 23:28:21 +0700

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,118 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from 'assets/img/jejak-logo.png';
+import { Facebook, Instagram, Twitter } from 'react-feather';
+import Text from 'components/Text';
+import colors from 'constants/colors';
+
+const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 2rem 0;
+  width: 100%;
+  align-items: center;
+`;
+
+const FooterBrand = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const FooterItemWrapper = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+`;
+
+const FooterItem = styled.li`
+  display: flex;
+  column-gap: 8px;
+  align-items: center;
+  a {
+    text-decoration: none;
+  }
+`;
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <FooterContainer>
+      <div className='container'>
+        <FooterBrand>
+          <img
+            src={Logo}
+            alt='jejak literasi logo'
+            width='100px'
+            height='80px'
+          />
+
+          <FooterItemWrapper>
+            <Text weight='700'>Navigasi</Text>
+            <FooterItem>
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Beranda</Text>
+              </a>
+            </FooterItem>
+            <FooterItem>
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Tentang Kami</Text>
+              </a>
+            </FooterItem>
+            <FooterItem>
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Program Kami</Text>
+              </a>
+            </FooterItem>
+            <FooterItem>
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Dokumentasi</Text>
+              </a>
+            </FooterItem>
+            <FooterItem>
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Kata Mereka</Text>
+              </a>
+            </FooterItem>
+            <FooterItem>
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Gabung Relawan</Text>
+              </a>
+            </FooterItem>
+          </FooterItemWrapper>
+          <FooterItemWrapper>
+            <Text weight='700'>Sosial Media</Text>
+            <FooterItem>
+              <Facebook size={20} />
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Jejak Literasi Bali</Text>
+              </a>
+            </FooterItem>
+            <FooterItem>
+              <Twitter size={20} />
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Jejak Literasi Bali</Text>
+              </a>
+            </FooterItem>
+            <FooterItem>
+              <Instagram size={20} />
+              <a href='facebook.com'>
+                <Text size='0.9rem'>Jejak Literasi Bali</Text>
+              </a>
+            </FooterItem>
+          </FooterItemWrapper>
+          <FooterItemWrapper>
+            <Text weight='700'>Alamat</Text>
+            <Text>
+              Jl. Pulau Saelus I No.18, Pedungan, Denpasar Selatan, Kota
+              Denpasar, Bali 80114
+            </Text>
+          </FooterItemWrapper>
+        </FooterBrand>
+      </div>
+      <Text color={colors.primary}>&copy; 2022 by Jejak Literasi Bali</Text>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
