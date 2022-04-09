0749fcd0dc6e43059b7d6d5c50a2f2946613b8c3
faisal isfahani <workwfaisal@gmail.com>
2022-04-10 02:50:50 +0700

    carousel responsive

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Slider from 'react-slick';
 import { CarouselItem } from './styled';
--- src/components/Carousel/styled.js
+++ src/components/Carousel/styled.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
+import Devices from 'utils/Devices';
 
 export const CarouselItem = styled.img`
   width: 100%;
+  ${Devices.phone} {
+    height: 350px;
+    object-fit: cover;
+  }
 `;
